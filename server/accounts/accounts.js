be877dd1428676ba9ddd26b1416eebb9fd9bf058
Fernando <lnfernandobr@gmail.com>
2023-08-26 11:40:57 -0400

    feat: using google secret keys from meteor settings, added a example settings as well

--- server/accounts/accounts.js
+++ server/accounts/accounts.js
@@ -8,19 +8,19 @@ Meteor.startup(() => {
     { service: 'google' },
     {
       $set: {
-        clientId: '',
+        clientId: Meteor.settings.google.clientId,
         loginStyle: 'popup',
-        secret: '',
+        secret: Meteor.settings.google.secret,
       },
     }
   );
 });
 
 Accounts.onCreateUser((user, originalUser) => ({
-    ...originalUser,
-    email: originalUser.services.google.email,
-    picture: originalUser.services.google.picture,
-    auth: {
-      accessToken: originalUser.services.google.accessToken,
-    },
-  }));
+  ...originalUser,
+  email: originalUser.services.google.email,
+  picture: originalUser.services.google.picture,
+  auth: {
+    accessToken: originalUser.services.google.accessToken,
+  },
+}));
