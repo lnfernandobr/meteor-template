f3ccd7335ae99cb169839db11aaef0396cb8f7b1
Vitor Flores <sf.joaovitor@gmail.com>
2024-01-28 19:27:48 -0400

    refactor: move files from /app folder to /client folder

--- client/general/AppContainer.js
+++ client/general/AppContainer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { LoggedUserProvider } from "./infra/user/LoggedUserProvider";
+import { LoggedUserProvider } from "../infra/user/LoggedUserProvider";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { DDPLink } from "@swydo/apollo-link-ddp";
-import { LocaleProvider } from "./infra/i18n/LocaleProvider";
+import { LocaleProvider } from "../infra/i18n/LocaleProvider";
 
-import "./infra/i18n/locales/pt_BR.i18n.json";
-import "./infra/i18n/locales/en.i18n.json";
+import "../infra/i18n/locales/pt_BR.i18n.json";
+import "../infra/i18n/locales/en.i18n.json";
 
 const client = new ApolloClient({
   link: new DDPLink(),
--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
-import { AppContainer } from "../app/AppContainer";
+import { AppContainer } from "./general/AppContainer";
 
 Meteor.startup(() => {
   const container = document.getElementById("app");
